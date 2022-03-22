e96cb4cdd4f3cfd6f8d122c87a338e264d3d52ec
parthsoni262 <parthsoni694@gmail.com>
2022-03-22 17:13:58 +0530

    Customized registration,login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   resources :portfolios, except: [:show]
 
   get 'angular-items', to: 'portfolios#angular'
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
       get :toggle_status
     end
   end
-    
+
   root to: 'pages#home'
 end
